318304546ec9db516565ca6ce6dd8426e21568a7
ayumitamai97 <ayumitamai97@gmail.com>
2018-08-03 01:44:38 +0900

    Finished creating template Created 2nd page and reordered styles

--- make.rb
+++ make.rb
@@ -2,6 +2,19 @@ require "PDFKit"
 require "erb"
 require "pry"
 
+PDFKit.configure do |config|
+  config.wkhtmltopdf = `which wkhtmltopdf`.to_s.strip
+  config.default_options = {
+    encoding:                "UTF-8",  # エンコーディング
+    page_size:               "A4",     # ページのサイズ
+    margin_top:              "0.25in", # 余白の設定
+    margin_right:            "1in",
+    margin_bottom:           "0.25in",
+    margin_left:             "1in",
+    disable_smart_shrinking: false
+  }
+end
+
 erb_file = File.open("pdf.html.erb").read
 rendered_html = ERB.new(erb_file).result
 
