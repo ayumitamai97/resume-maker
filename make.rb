19c35592c86ae93397e409c1d2422d15261f59a6
ayumitamai97 <ayumitamai97@gmail.com>
2018-08-03 01:55:31 +0900

    Set page-break-before: always;

--- make.rb
+++ make.rb
@@ -7,9 +7,9 @@ PDFKit.configure do |config|
   config.default_options = {
     encoding:                "UTF-8",  # エンコーディング
     page_size:               "A4",     # ページのサイズ
-    margin_top:              "0.25in", # 余白の設定
+    margin_top:              "1in", # 余白の設定
     margin_right:            "1in",
-    margin_bottom:           "0.25in",
+    margin_bottom:           "1in",
     margin_left:             "1in",
     disable_smart_shrinking: false
   }
@@ -18,7 +18,7 @@ end
 erb_file = File.open("pdf.html.erb").read
 rendered_html = ERB.new(erb_file).result
 
-file = open("RubyPDFExample.pdf","w")
+file = open("resume_template.pdf","w")
 file.puts PDFKit.new(rendered_html).to_pdf
 file.close
 
