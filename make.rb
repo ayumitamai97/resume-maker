883bead9be0d4776d123ebca908b58a3649e40dd
ayumitamai97 <ayumitamai97@gmail.com>
2018-08-02 18:10:23 +0900

    Changed html file into html.erb

--- make.rb
+++ make.rb
@@ -1,14 +1,12 @@
 require "PDFKit"
 require "erb"
+require "pry"
 
-erb_file = File.open("pdf.html")
-# erb_file = File.open("pdf.html.erb")
-
-# rendered_html = ERB.new(erb_file).result
+erb_file = File.open("pdf.html.erb").read
+rendered_html = ERB.new(erb_file).result
 
 file = open("RubyPDFExample.pdf","w")
-# file.puts PDFKit.new(erb_file).to_pdf
-file.puts PDFKit.new(erb_file).to_pdf
+file.puts PDFKit.new(rendered_html).to_pdf
 file.close
 
 
